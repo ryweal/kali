1c6f0c3bd9c1b770581c24d27d862f0d616a7987
ryweal <ryweal39@gmail.com>
2024-05-20 22:32:20 +0200

    Improve static serve

--- src/file.ts
+++ src/file.ts
@@ -1,96 +1,183 @@
-import { createReadStream, readFile, stat } from 'fs';
+import { createReadStream, stat } from 'node:fs';
 import { HttpResponse } from 'uWebSockets.js';
 import { contentType } from 'mime-types';
-import { extname } from 'path';
-import { sendInternalServerError, sendHeaders } from './utils';
-import { KaliSendFileOptions, RangeHeader } from './types';
-import { constants } from 'http2'
+import { extname } from 'node:path';
+import { sendHeaders } from './utils';
+import { RangeHeader } from './types';
+import { constants } from 'node:http2';
 
-const parseRangeHeader = (range: RangeHeader): { start: number, end: number } => {
-    let [start, end] = range.substring(6).split('-', 2).map((e: string) => +e);
-    return {
-        start: isNaN(start) ? 0 : start,
-        end: isNaN(end) ? 0 : end,
-    };
-}
+export const parseRangeHeader = (
+  range: RangeHeader,
+): { start: number; end?: number } => {
+  let [start, end] = range
+    .substring(6)
+    .split('-', 2)
+    .map((e: string) => +e);
+  return {
+    start: isNaN(start) ? 0 : start,
+    end: isNaN(end) ? undefined : end,
+  };
+};
 
-const sizeFromRange = (
-    range: RangeHeader | { start: number, end: number } | undefined,
-    chunkSize: number
-): { start: number, end: number } => {
-    if (range === undefined) {
-        return { start: 0, end: chunkSize }
-    }
-
-    if (typeof range === 'string') {
-        return parseRangeHeader(range);
-    }
-
-    return range
-}
-
-export const sendFile = (
-    res: HttpResponse,
-    path: string,
-    range: RangeHeader | { start: number, end: number } | undefined,
-    options: KaliSendFileOptions,
-    callback: (err: unknown) => void
-): void => {
-    const chunkSize = options?.chunkSize ?? 4 * 1024 * 1024;
-    let { start, end } = sizeFromRange(range, chunkSize)
-    let buffer: Buffer
-    stat(path, (err, stats) => {
-        if (err) {
-            callback(err)
-            return;
+const sendFromStream = (
+  res: HttpResponse,
+  path: string,
+  start: number,
+  end: number,
+  size: number,
+  headers: Record<string, string> | undefined,
+  highWaterMark: number,
+  callback: (error?: unknown) => void,
+) => {
+  if (res.aborted) {
+    return callback('aborted');
+  }
+  let isFirst = true;
+  createReadStream(path, {
+    start,
+    end,
+    highWaterMark,
+  })
+    .on('data', (chunk) => {
+      if (res.aborted) {
+        return callback('aborted');
+      }
+      res.cork(() => {
+        if (isFirst) {
+          res.writeStatus('206');
+          res.writeHeader(
+            constants.HTTP2_HEADER_RANGE,
+            `bytes=${start}-${end - 1}/${size}`,
+          );
+          res.writeHeader(
+            constants.HTTP2_HEADER_CONTENT_TYPE,
+            contentType(extname(path)) || 'application/octet-stream',
+          );
+          if (headers) {
+            sendHeaders(res, headers);
+          }
+          isFirst = false;
         }
-        if (range === undefined && stats.size <= chunkSize) {
-            readFile(path, (err, buffer) => {
-                if (err) {
-                    callback(err)
-                    return;
-                }
-                if (res.aborted) {
-                    return;
-                }
-                res.cork(() => {
-                    res.writeStatus(`${ options?.statusCode ?? 200 }`);
-                    res.writeHeader(constants.HTTP2_HEADER_CONTENT_TYPE, options?.contentType || contentType(extname(path)) || 'application/octet-stream');
-                    if (options?.headers) {
-                        sendHeaders(res, options.headers);
-                    }
-                    res.end(buffer);
-                });
-            })
-            return;
-        }
-        try {
-            end = Math.min(start + chunkSize, stats.size);
-            createReadStream(path, {
-                start,
-                end,
-                highWaterMark: chunkSize
-            }).on('data', chunk => {
-                const chunkBuffer = typeof chunk === 'string' ? Buffer.from(chunk) : chunk;
-                buffer = buffer ? Buffer.concat([buffer, chunkBuffer]) : chunkBuffer
-            }).on('end', () => {
-                if (res.aborted) {
-                    return;
-                }
-                res.cork(() => {
-                    res.writeStatus('206')
-                    res.writeHeader(constants.HTTP2_HEADER_CONTENT_RANGE, `bytes ${start}-${end-1}/${stats.size}`)
-                    res.writeHeader(constants.HTTP2_HEADER_CONTENT_TYPE, options?.contentType || contentType(extname(path)) || 'application/octet-stream')
-                    if (options?.headers) {
-                        sendHeaders(res, options.headers);
-                    }
-                    res.end(buffer);
-                })
-            }).on('error', () => {
-                sendInternalServerError(res)
-            })
-        } catch (err: unknown) {
-            sendInternalServerError(res)
-        }
+        res.write(chunk);
+      });
     })
-}
+    .on('end', () => {
+      if (res.aborted) {
+        return callback('aborted');
+      }
+      res.cork(() => {
+        res.end();
+      });
+      callback();
+    })
+    .on('error', callback);
+};
+
+const sendFileRange = (
+  res: HttpResponse,
+  path: string,
+  range: { start: number; end?: number },
+  options: {
+    highWaterMark: number;
+    rangeSize: number;
+    headers?: Record<string, string>;
+  },
+  callback: (err: unknown) => void,
+) => {
+  stat(path, (err, stats) => {
+    if (err) {
+      return callback(err);
+    }
+    if (range.start > stats.size) {
+      return callback('Out of range');
+    }
+    sendFromStream(
+      res,
+      path,
+      range.start,
+      range.end ?? options.rangeSize,
+      stats.size,
+      options.headers,
+      options.highWaterMark,
+      callback,
+    );
+  });
+};
+
+const sendFullFile = (
+  res: HttpResponse,
+  path: string,
+  options: { highWaterMark: number; headers?: Record<string, string> },
+  callback: (err?: unknown) => void,
+) => {
+  let isFirst = true;
+  createReadStream(path, {
+    highWaterMark: options.highWaterMark,
+  })
+    .on('data', (chunk) => {
+      if (res.aborted) {
+        return callback('aborted');
+      }
+      res.cork(() => {
+        if (isFirst) {
+          res.writeStatus('200');
+          res.writeHeader(
+            constants.HTTP2_HEADER_CONTENT_TYPE,
+            contentType(extname(path)) || 'application/octet-stream',
+          );
+          if (options.headers) {
+            sendHeaders(res, options.headers);
+          }
+          isFirst = false;
+        }
+        res.write(chunk);
+      });
+    })
+    .on('end', () => {
+      if (res.aborted) {
+        return callback('aborted');
+      }
+      res.cork(() => {
+        res.end();
+      });
+      callback();
+    })
+    .on('error', callback);
+};
+export const sendFile = (
+  res: HttpResponse,
+  path: string,
+  range: { start: number; end?: number } | undefined,
+  options: {
+    highWaterMark?: number;
+    rangeSize?: number;
+    headers?: Record<string, string>;
+  },
+  callback: (err?: unknown) => void,
+) => {
+  const rangeSize = options.rangeSize ?? 4 * 1024 * 1024;
+  const highWaterMark = options.highWaterMark ?? 4 * 1024 * 1024;
+  if (range) {
+    sendFileRange(
+      res,
+      path,
+      range,
+      {
+        highWaterMark,
+        rangeSize,
+        headers: options.headers,
+      },
+      callback,
+    );
+  } else {
+    sendFullFile(
+      res,
+      path,
+      {
+        highWaterMark,
+        headers: options.headers,
+      },
+      callback,
+    );
+  }
+};
--- src/static-route-config.ts
+++ src/static-route-config.ts
@@ -1,75 +1,115 @@
 import { TemplatedApp } from 'uWebSockets.js';
 import { createStaticRouteHandler } from './static-route-handler';
 import {
-    AnyKaliStaticConfigBuilder,
-    AsyncGuard,
-    Guard,
-    KaliRouter,
-    KaliStaticConfigBuilder, NextKaliStaticConfigBuilder, StaticOptions,
-    StaticRouteConfig,
-    Validator,
+  AnyKaliStaticConfigBuilder,
+  AsyncGuard,
+  Guard,
+  KaliRouter,
+  KaliStaticConfigBuilder,
+  NextKaliStaticConfigBuilder,
+  StaticOptions,
+  StaticRouteConfig,
+  Validator,
 } from './types';
 
 export class StaticConfigBuilder<
-    State extends KaliRouter<any>,
-    Context = undefined,
-    Query = {},
-    Headers = {},
-    Params = {},
-    Consumed extends keyof AnyKaliStaticConfigBuilder | '' = ''
-> implements KaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed> {
-    private readonly config: StaticRouteConfig = {
-        validateHeaders: undefined,
-        validateQuery: undefined,
-        validateParams: undefined,
-        guard: undefined,
-        index: undefined,
-        redirectNotFoundToIndex: false,
-        rootDirectory: '',
-        chunkSize: undefined
-    }
+  State extends KaliRouter<any>,
+  Context = undefined,
+  Query = {},
+  Headers = {},
+  Params = {},
+  Consumed extends keyof AnyKaliStaticConfigBuilder | '' = '',
+> implements
+    KaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed>
+{
+  private readonly config: StaticRouteConfig = {
+    validateHeaders: undefined,
+    validateQuery: undefined,
+    validateParams: undefined,
+    guard: undefined,
+    index: undefined,
+    redirectNotFoundToIndex: false,
+    rootDirectory: '',
+    rangeSize: undefined,
+    highWaterMark: undefined,
+  };
 
-    constructor(
-        private readonly router: State,
-        private readonly app: TemplatedApp,
-        private readonly context: Context,
-        private readonly pattern: string
-    ) {
-    }
+  constructor(
+    private readonly router: State,
+    private readonly app: TemplatedApp,
+    private readonly context: Context,
+    private readonly pattern: string,
+  ) {}
 
-    public headers<Headers>(headers: Validator<Headers, Record<string, string>>) {
-        this.config.validateHeaders = headers;
+  public headers<Headers>(headers: Validator<Headers, Record<string, string>>) {
+    this.config.validateHeaders = headers;
 
-        return this as unknown as NextKaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed | 'headers'>
-    }
+    return this as unknown as NextKaliStaticConfigBuilder<
+      State,
+      Context,
+      Query,
+      Headers,
+      Params,
+      Consumed | 'headers'
+    >;
+  }
 
-    public params<Params>(params: Validator<Params, Record<string, string>>) {
-        this.config.validateParams = params;
+  public params<Params>(params: Validator<Params, Record<string, string>>) {
+    this.config.validateParams = params;
 
-        return this as unknown as NextKaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed | 'params'>
-    }
+    return this as unknown as NextKaliStaticConfigBuilder<
+      State,
+      Context,
+      Query,
+      Headers,
+      Params,
+      Consumed | 'params'
+    >;
+  }
 
-    public query<Query>(query: Validator<Query, URLSearchParams>) {
-        this.config.validateQuery = query;
+  public query<Query>(query: Validator<Query, URLSearchParams>) {
+    this.config.validateQuery = query;
 
-        return this as unknown as NextKaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed | 'query'>
-    }
+    return this as unknown as NextKaliStaticConfigBuilder<
+      State,
+      Context,
+      Query,
+      Headers,
+      Params,
+      Consumed | 'query'
+    >;
+  }
 
-    public guard(guard: Guard<Context, Query, Headers, Params, undefined, undefined> | AsyncGuard<Context, Query, Headers, Params, undefined, undefined>) {
-        this.config.guard = guard;
+  public guard(
+    guard:
+      | Guard<Context, Query, Headers, Params, undefined, undefined>
+      | AsyncGuard<Context, Query, Headers, Params, undefined, undefined>,
+  ) {
+    this.config.guard = guard;
 
-        return this as unknown as NextKaliStaticConfigBuilder<State, Context, Query, Headers, Params, Consumed | 'query' | 'headers' | 'params' | 'guard'>
-    }
+    return this as unknown as NextKaliStaticConfigBuilder<
+      State,
+      Context,
+      Query,
+      Headers,
+      Params,
+      Consumed | 'query' | 'headers' | 'params' | 'guard'
+    >;
+  }
 
-    public serve(options: StaticOptions): State {
-        this.config.rootDirectory = options.rootDirectory;
-        this.config.index = options.index;
-        this.config.redirectNotFoundToIndex = options.redirectNotFoundToIndex ?? false;
-        this.config.chunkSize = options.chunkSize
+  public serve(options: StaticOptions): State {
+    this.config.rootDirectory = options.rootDirectory;
+    this.config.index = options.index;
+    this.config.redirectNotFoundToIndex =
+      options.redirectNotFoundToIndex ?? false;
+    this.config.rangeSize = options.rangeSize;
+    this.config.highWaterMark = options.highWaterMark;
 
-        this.app.get(`${this.pattern}*`, createStaticRouteHandler(this.context, this.pattern, this.config))
+    this.app.get(
+      `${this.pattern}*`,
+      createStaticRouteHandler(this.context, this.pattern, this.config),
+    );
 
-        return this.router;
-    }
-
+    return this.router;
+  }
 }
